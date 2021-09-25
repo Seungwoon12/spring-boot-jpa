9b82de24b00b1dc1fd06417d1148201ff62e6554
Seungwoon <swdev678@gmail.com>
2021-09-25 21:31:16 +0900

    jpa 학습

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,15 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+
+    // 걍 JPA 스펙상 protected로 기본 생성자 만들어준다.
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
