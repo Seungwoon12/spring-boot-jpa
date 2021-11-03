b89658e82a9ed648e3765bdab0fc09355b56afe9
Seungwoon <swdev678@gmail.com>
2021-11-03 23:46:52 +0900

    jpa 학습

--- jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,12 +1,18 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.UpdateItemDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,5 +39,47 @@ public class ItemController {
 
         itemService.saveItem(book);
         return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+
+        // 아래 주석처리 한 것처럼 코드 짜는건 안좋은 방식
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+
+        // 만약 업데이트 칠 항목이 너무 많다면 Dto 클래스를 만들어서 처리하는 것이 깔끔하고 좋음.
+//        itemService.updateItem(itemId, UpdateItemDto itemDto); // 이런식으로
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
+
+        return "redirect:/items";
     }
 }
--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +24,23 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    /**
+     * 엔티티를 변경할때는 항상 변경감지를 사용해라. merge는 사용하지마라
+     */
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        /**
+         * 아래와 같이만 해줘도 JPA가 알아서 엔티티의 변경을 감지하고 update를 쳐줌. 변경감지 방식임.
+         * 내가 update 칠 필드들만 set해줘야함. merge는 set으로 변경 안해준 필드의 값을 null로 바꿔 넣기 때문에 위험함
+         * 이렇게 트랜잭션 안에서 엔티티를 조회해야 영속 상태로 조회가 됨. 그리고 그 값을 변경해야 더티체킹(변경감지)이 일어나고 트랜잭션이 커밋될 때 flush가 일어나면서 db에 업데이트 쿼리가 날라감
+         */
+        Item findItem = itemRepository.findOne(itemId);
+//        findItem.change(price, name, stockQuantity); // 실제로는 이런식으로 메서드 만들어서 변경하기. 그래야 추적 및 관리하기 쉬움
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
